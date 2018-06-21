3d45c5dc4cf0140f510573559fb7dfeebb87147c
hyperking <derryspann@gmail.com>
2018-06-21 19:28:08 -0400

    initial commit

---
+++ derry_plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:       DERRY API 
+ * Plugin URI:        http://github.com/hyperking/derry-api.git
+ * Description:       This plugin grabs posts from aggregate sites
+ * Version:           1.0
+ * Author:            Derry Spann
+ * Author URI:        http://derryspann.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       derry-api
+ * Domain Path:       /languages
+ */
